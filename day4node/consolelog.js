14e08b431d64be61531b466030d0bf85eb6435ab
Tegutegu <46686759+Tegutegu@users.noreply.github.com>
2019-09-11 15:34:24 -0500

    Console setup coding for node.js

---
+++ day4node/consolelog.js
@@ -0,0 +1,10 @@
+console.log('1 : Greetings!');
+console.log("2 : this works as well");
+
+var str = 'nodejsera';
+var val = 25;
+
+console.log(str);
+console.log('4: Value of val is : %d' , val);
+console.log('5: %s', 'this will be printed after 5');
+console.log("6 : str =" + str);
