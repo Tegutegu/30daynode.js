fcc71fd8cbe538237f74732896004aeac366382a
Tegutegu <46686759+Tegutegu@users.noreply.github.com>
2019-09-16 16:17:51 -0500

    package publishing on npm website and account

---
+++ day8node/packagepublish.js
@@ -0,0 +1,28 @@
+var function_name_1 = function (parameters){
+						
+    Function code starts
+    ...				...
+    ...				...
+    Functionality
+    ...  			...
+    ...				...
+    Function ends here 
+    }
+var function_name_2 = function (parameters){
+
+        
+    Function code starts
+    ...				...
+    ...				...
+    Functionality
+    ...  			...
+    ...				...
+    Function ends here 				
+    }
+module.exports = {
+function_1 : function_name_1,
+function_2 : function_name_2
+}; 
+
+//export method
+//needs a npm account for publishing
