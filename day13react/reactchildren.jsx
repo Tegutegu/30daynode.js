a042916a83379007b4c84b2bba36478d00b896ce
Tegutegu <46686759+Tegutegu@users.noreply.github.com>
2019-09-18 13:18:18 -0500

    repeating elements and children tutorial

---
+++ day13react/reactchildren.jsx
@@ -0,0 +1,21 @@
+const Formatter = ({format, state}) => {
+    let children = format.split('').map(e => {
+      if (e == 'h') {
+        return <Hour />
+      } else if (e == 'm') {
+        return <Minute />
+      } else if (e == 's') {
+        return <Second />
+      } else if (e == 'p') {
+        return <Ampm />
+      } else if (e == ' ') {
+        return <span> </span>;
+      } else {
+        return <Separator />
+      }
+    });
+    return (<span>
+        {React.Children
+          .map(children, c => React.cloneElement(c, state))}
+        </span>)
+  }
---
+++ day13react/reactcloneelement.jsx
@@ -0,0 +1,4 @@
+React.createElement("div", null, 
+ React.createElement("img", {src: "profile.jpg", alt: "Profile photo"}),
+ React.createElement("h1", null, "Welcome back Ari")
+);
