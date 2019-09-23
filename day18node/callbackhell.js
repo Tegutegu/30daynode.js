a7f1a6c6b745c761483958c8f022ac4724307842
Tegutegu <46686759+Tegutegu@users.noreply.github.com>
2019-09-23 12:21:53 -0500

    callbacks tutorial

---
+++ day18node/callbackhell.js
@@ -0,0 +1,21 @@
+//avoid this 
+
+fxn1(function(){
+    fxn2(function(){
+        fxn3(function(){
+            fxn4(function(){
+				fxn5(function(){
+					fxn6(function(){
+						fxn7(function(){
+							fxn8(function(){
+								....
+            
+			
+							});
+						});
+					});
+				});
+			});
+        });
+    });
+});
